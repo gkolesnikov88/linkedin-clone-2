49717e559f88334bca47e621ee9948a235bad1dc
George Kolesnikov <gkolesnikov88@gmail.com>
2024-06-30 09:12:06 +0200

    Add user information aside box

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import UserInformation from "@/components/UserInformation";
 
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Linkdin Clone</h1>
+    <main className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:col-span-2">
+        <UserInformation />
+      </section>
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+        <div className="bg-red-500">PostForm</div>
+        {/* PostForm */}
+        {/* PostFeed */}
+      </section>
+      <section className="hidden xl:inline justify-center col-span-2">
+        {/* Widget */}
+        <div className="bg-blue-200">Widget</div>
+      </section>
     </main>
   );
 }
