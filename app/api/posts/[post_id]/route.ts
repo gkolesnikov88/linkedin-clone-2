0410b5f8007e5c8a9fde8fb178144653d6b55d84
George Kolesnikov <gkolesnikov88@gmail.com>
2024-07-08 06:32:56 +0200

    Add Post API. Get one Delete one

---
+++ app/api/posts/[post_id]/route.ts
@@ -0,0 +1,59 @@
+import connectDB from "@/mongodb/db";
+import { Post } from "@/mongodb/models/post";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(
+//   request: Request,
+  { params }: { params: { post_id: string } }
+) {
+  await connectDB();
+
+  try {
+    const post = await Post.findById(params.post_id);
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+    return NextResponse.json(post);
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Error fetching post: ${error}` },
+      { status: 500 }
+    );
+  }
+}
+
+export interface DeletePostRequesBody {
+  userId: string;
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { post_id: string } }
+) {
+  auth().protect(); // protect the route with Clerk authentication
+  await connectDB();
+
+//   const user = await currentUser();
+  const { userId }: DeletePostRequesBody = await request.json();
+
+  try {
+    const post = await Post.findById(params.post_id);
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+    // if (post.user.userId !== user?.id) {
+    if (post.user.userId !== userId) {
+      // throw new Error("Unauthorized");
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await post.removePost();
+    return NextResponse.json({ message: "Post deleted successfully" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Error deleting post: ${error}` },
+      { status: 500 }
+    );
+  }
+}
