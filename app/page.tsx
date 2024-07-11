a04524c59fc0a3d424ad8de5cf45873113c26ef9
George Kolesnikov <gkolesnikov88@gmail.com>
2024-07-11 07:17:48 +0200

    Fetch posts. Show PostForm only for signed user

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 import PostForm from "@/components/PostForm";
 import UserInformation from "@/components/UserInformation";
+import connectDB from "@/mongodb/db";
+import { Post } from "@/mongodb/models/post";
+import { SignedIn } from "@clerk/nextjs";
 
+export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  await connectDB();
+  
+  const posts = await Post.getAllPosts();
+  console.log(posts);
+
   return (
     <main className="grid grid-cols-8 mt-5 sm:px-5">
       <section className="hidden md:inline md:col-span-2">
         <UserInformation />
       </section>
       <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
-        <PostForm />
+        <SignedIn>
+          <PostForm />
+        </SignedIn>
         {/* PostFeed */}
       </section>
       <section className="hidden xl:inline justify-center col-span-2">
