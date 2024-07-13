51daac79c43f181c3dcddb38e4f1cb571bed7ca7
George Kolesnikov <gkolesnikov88@gmail.com>
2024-07-13 18:55:33 +0200

    Add the Widget component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import PostFeed from "@/components/PostFeed";
 import PostForm from "@/components/PostForm";
 import UserInformation from "@/components/UserInformation";
+import Widget from "@/components/Widget";
 import connectDB from "@/mongodb/db";
 import { Post } from "@/mongodb/models/post";
 import { SignedIn } from "@clerk/nextjs";
@@ -25,8 +26,7 @@ export default async function Home() {
         <PostFeed posts={posts} />
       </section>
       <section className="hidden xl:inline justify-center col-span-2">
-        {/* Widget */}
-        <div className="bg-blue-200">Widget</div>
+        <Widget />
       </section>
     </main>
   );
---
+++ components/Widget.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Widget() {
+  return (
+    <div
+      className="ml-6 max-h-[790px] flex flex-col justify-center 
+        items-center bg-white rounded-lg border py-4"
+    >
+      <h1 className="text-xl font-semibold">Latest news</h1>
+      <div>New versions</div>
+      <div>Change the world</div>
+    </div>
+  );
+}
+
+export default Widget;
